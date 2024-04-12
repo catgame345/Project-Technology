deaf41d8431ffbf71bda3469a9701eef18a8215e
catgame345 <eduardoadlercruz@gmail.com>
2024-04-12 13:03:38 -0600

    first commit

---
+++ src/routes/(common)/practica1/+page.svelte
@@ -0,0 +1,28 @@
+<main>
+    <h1>Practica 1</h1>
+    <h2>Infografía acerca del internet</h2>
+    <section>
+        <img src="src/routes/(common)/practica1.png" />
+        <h3>Creado por: <em>Eduardo Adler Cruz Velázquez</em></h3>
+    </section>
+</main>
+
+<style>
+    main {
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        text-align: center;
+    }
+    img {
+        width: 400px;
+        height: 440px;
+        padding-top: 20px;
+    }
+    section {
+        background-color: rgb(29 78 216);
+        margin-right: 440px;
+        margin-left: 440px;
+    }
+    h3 {
+        padding-bottom: 20px;
+    }
+</style>
---
+++ src/routes/(common)/practica2/+page.svelte
@@ -0,0 +1,10 @@
+<main>
+    <h1>Proximamente...</h1>
+</main>
+
+<style>
+    main {
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        text-align: center;
+    }
+</style>
