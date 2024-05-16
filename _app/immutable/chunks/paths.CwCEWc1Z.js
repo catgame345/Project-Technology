e08a4dd0c1b72bc43c965c3957c5133ccbbbfb1f
catgame345 <eduardoadlercruz@gmail.com>
2024-05-16 17:18:39 -0600

    Updates

--- _app/immutable/chunks/paths.CwCEWc1Z.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_14a62je)==null?void 0:s.base)??"/Project-Technology";var e;const t=((e=globalThis.__sveltekit_14a62je)==null?void 0:e.assets)??a;export{t as a,a as b};
---
+++ _app/immutable/entry/start.BlL97Kza.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DPE8Y1_O.js";export{t as start};
