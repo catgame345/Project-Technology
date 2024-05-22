3725132632f9b13e7e4ed49b353e641f17097e07
catgame345 <eduardoadlercruz@gmail.com>
2024-05-22 15:54:03 -0600

    Updates

--- _app/immutable/chunks/paths.BJHgDpST.js
+++
@@ -1,1 +0,0 @@
-var s;const l=((s=globalThis.__sveltekit_11qclle)==null?void 0:s.base)??"/Project-Technology";var e;const t=((e=globalThis.__sveltekit_11qclle)==null?void 0:e.assets)??l;export{t as a,l as b};
