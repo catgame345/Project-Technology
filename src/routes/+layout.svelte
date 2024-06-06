05214269cde1597142d36ab1f92b179dc9fe3368
catgame345 <eduardoadlercruz@gmail.com>
2024-06-06 16:50:32 -0600

    Fake News

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -116,6 +116,22 @@
         </svg>
       </button>
     </a>
+    <a href="{base}/practica7">
+      <button class="item">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          fill="currentColor"
+          class="bi bi-7-circle-fill"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.37 5.11h3.972v.07L6.025 12H7.42l3.258-6.85V4.002H5.369v1.107Z"
+          />
+        </svg>
+      </button>
+    </a>
     <a href="{base}/practica8">
       <button class="item">
         <svg
